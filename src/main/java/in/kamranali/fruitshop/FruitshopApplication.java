2ebf06c1260e48709c7a6609562dd8585325ed18
Kamran Ali <kamranalinitb@gmail.com>
2017-10-19 19:17:00 +0530

    Initial commit

---
+++ src/main/java/in/kamranali/fruitshop/FruitshopApplication.java
@@ -0,0 +1,12 @@
+package in.kamranali.fruitshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitshopApplication.class, args);
+	}
+}
